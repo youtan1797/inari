3c8abb4550242d86e1b63367bb6f10d9508e4ca1
youtan1797 <efunazuri@outlook.com>
2025-06-22 07:36:34 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const toggleBtn = document.querySelector('.open-volunteer');
-  const box = document.querySelector('.volunteer-box');
-
-  toggleBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    box.classList.toggle('hidden');
-  });
-});
